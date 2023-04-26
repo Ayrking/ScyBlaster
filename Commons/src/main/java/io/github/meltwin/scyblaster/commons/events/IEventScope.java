8f7f5f58ff0bbb3308f1f58e63bc2f5da8eac8e5
Meltwin <meltwin158@gmail.com>
2023-04-26 13:24:33 +0200

    Rebase everything

--- Commons/src/main/java/io/github/meltwin/scyblaster/commons/events/IEventScope.java
+++
@@ -1,9 +0,0 @@
-package io.github.meltwin.scyblaster.commons.events;
-
-/**
- * Interface for events scope
- * @Copyright: GNU APGLv3 - (C) 2022 Meltwin
- * @author Meltwin
- * @since 0.1-SNAPSHOT
- */
-public interface IEventScope {}
--- Launcher/src/main/java/io/github/meltwin/scyblaster/launcher/Launcher.java
+++
@@ -1,6 +0,0 @@
-package io.github.meltwin.scyblaster.launcher;
-
-public class Launcher {
-
-    public static void main(String[] args) {new Launcher();}
-}
