3d09c9230c6de3118d140042a272e9f36ea66fd1
G Côte <meltwin158@gmail.com>
2023-06-17 19:15:58 +0200

    Adding bootstrap

---
+++ bootstrap/src/main/java/io/meltwin/scyblaster/bootstrap/Bootstrap.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Meltwin 2023.
+ *
+ * Scyblaster Bootstrap is a Minecraft-dedicated file checker
+ *
+ * Source: https://github.com/Meltwin/ScyBlaster
+ * Distributed under the GNU Affero General Public License v3.0
+ */
+
+package io.meltwin.scyblaster.bootstrap;
+
+import io.meltwin.scyblaster.common.io.ResourceManager;
+
+public class Bootstrap {
+    static ResourceManager file_manager = new ResourceManager();
+}
